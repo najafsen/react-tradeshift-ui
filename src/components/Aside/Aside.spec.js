8ba8765d54937820ed8615389a9c419043a56c9f
jason.gu <jason.gu@tradeshift.com>
2019-07-17 09:52:20 +0800

    style(Aside): improve the way of importing packages

--- src/components/Aside/Aside.spec.js
+++ src/components/Aside/Aside.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Aside from './index';
+import Aside from '.';
 
 configure({ adapter: new Adapter() });
 
