d8d87da4295879ae8dc3b815709726a24c2be3d2
jason.gu <jason.gu@tradeshift.com>
2019-09-23 11:52:52 +0800

    feat(storybook): add the usage of the linkable table

--- src/stories/Table.js
+++ src/stories/Table.js
@@ -4,15 +4,62 @@ import { boolean } from '@storybook/addon-knobs';
 import { Table } from '../components';
 
 const stories = storiesOf('Table', module);
-const tableConfig = {
-	cols: [{ label: 'One' }, { label: 'Two' }, { label: 'Three' }],
-	rows: [['A', 'D', 'G'], ['B', 'E', 'H'], ['C', 'F', 'I']]
-};
 stories.add('Basic usage', () => {
+	const tableConfig = {
+		cols: [{ label: 'One' }, { label: 'Two' }, { label: 'Three' }],
+		rows: [['A', 'D', 'G'], ['B', 'E', 'H'], ['C', 'F', 'I']]
+	};
 	const shouldShow = boolean('shouldShow', false);
 	return shouldShow ? (
 		<div style={{ height: 200 }}>
 			<Table id="profile-table" cols={tableConfig.cols} rows={tableConfig.rows} />
+		</div>
+	) : null;
+});
+stories.add('Linkable table element', () => {
+	const tableConfig = {
+		cols: [{ label: 'name' }, { label: 'gender' }, { label: 'action' }],
+		rows: [
+			[
+				'Tom',
+				'Male',
+				{
+					text: '[sayHello](sayHello)',
+					type: 'say-hello'
+				}
+			],
+			[
+				'Lucy',
+				'Female',
+				{
+					text: '[sayHello](sayHello)',
+					type: 'say-hello'
+				}
+			],
+			[
+				'Li Lei',
+				'Male',
+				{
+					text: '[sayHello](sayHello)',
+					type: 'say-hello'
+				}
+			]
+		]
+	};
+	const handleLinkable = action => {
+		if (action === 'sayHello') {
+			window.ts.ui.Notification.info('hello');
+		}
+	};
+	const shouldShow = boolean('shouldShow', false);
+	return shouldShow ? (
+		<div style={{ height: 200 }}>
+			<Table
+				id="profile-table"
+				cols={tableConfig.cols}
+				rows={tableConfig.rows}
+				linkable={handleLinkable}
+			/>
 		</div>
 	) : null;
 });
